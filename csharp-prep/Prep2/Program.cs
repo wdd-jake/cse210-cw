ef29f475b610800113f95bb439635200214b6cd4
wdd-jake <jakenimor666@live.com>
2026-01-09 15:41:30 -0800

    prep1 and prep2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -5,5 +5,43 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep2 World!");
+
+        Console.Write("What is your grade percentage? ");
+        string number = Console.ReadLine();
+        int percent = int.Parse(number);
+
+        string lettergrade = "";
+
+        if (percent >= 90)
+        {
+            lettergrade = "A";
+        }
+        else if (percent >= 80)
+        {
+            lettergrade = "B";
+        }
+        else if (percent >= 70)
+        {
+            lettergrade = "C";
+        }
+        else if (percent >= 60)
+        {
+            lettergrade = "D";
+        }
+        else
+        {
+            lettergrade = "F";
+        }
+
+        Console.WriteLine($"Your grade is: {lettergrade}");
+        
+        if (percent >= 70)
+        {
+            Console.WriteLine("You passed!");
+        }
+        else
+        {
+            Console.WriteLine("Better luck next time!");
+        }
     }
 }
